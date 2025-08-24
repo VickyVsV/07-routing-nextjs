8abd59378210ea809a0f1adc8e4e63aaa0a4a874
VickyVsV <vikusiavsv@gmail.com>
2025-08-24 23:37:51 +0300

    fixed for versel

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,13 +1,16 @@
 import { fetchNotes } from '@/lib/api';
 import Notes from './Notes.client';
 
-export default async function NotesPage({ params }: { params: { slug?: string[] } }) {
+type Props = {
+    params: Promise <{slug: string[]}>
+}
+
+export default async function NotesPage({ params }: Props) {
+  /* const tag = params.slug?.[0] || null; */
+  const { slug } = await params;
+  const tag = slug?.[0] === 'all' ? null : slug?.[0] ?? null;
   // SSR загрузка первой страницы без поиска
   const initialData = await fetchNotes('', 1, 12);
-  /* const tag = params.slug?.[0] || null; */
-  const resolvedParams = await params;
-  const slug = resolvedParams?.slug;
-  const tag = slug?.[0] === 'all' ? null : slug?.[0] ?? null;
 
   return <Notes initialData={initialData} tag={tag} />;
 }
